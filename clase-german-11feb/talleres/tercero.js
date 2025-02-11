25bc8de8969fe7eba154163220d943e25459a00a
DiriRH19 <rdirison@gmail.com>
2025-02-11 17:05:06 -0500

    trimestre 5

--- clase-german-11feb/talleres/tercero.js
+++ clase-german-11feb/talleres/tercero.js
@@ -3,18 +3,32 @@
 //  Usar tres promesas y encadenarlas con .then() para que se ejecuten en orden.
 
 const sumar = (a, b) => {
-    return new Promise((resolve) => {
-    resolve(a + b);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+        resolve(a + b);
+    }, 1000);
+    
+  });
 };
 
-sumar(3, 5).then((resultado) => {
+sumar(3, 5)
+  .then((resultado) => {
     console.log(`El resultado es: ${resultado}`);
-     // El resultado es: 8
-    return sumar(resultado, 10); //Nueva promesa
-}).then((nuevoResultado) => {
-    console.log(`El nuevo resultado es: ${nuevoResultado}`); 
-    // El nuevo resultado es: 18
-}).catch((error) => {
+    // El resultado es: 8
+    return sumar(resultado, 10);
+  })
+
+  .then((nuevoresultado)=>{
+    console.log(`El nuevo resultado es: ${nuevoresultado}`);
+        return sumar(nuevoresultado,2)
+  })
+
+  .then((nuevoNuevoResultado) => {
+        console.log(`El nuevo resultado es: ${nuevoNuevoResultado}`);
+        // El nuevo resultado es: 18
+  })
+  .catch((error) => {
     console.error(error);
-});
+  });
+
+      
