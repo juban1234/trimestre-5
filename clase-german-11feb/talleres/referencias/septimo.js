ab74db6f12aa2aa5dc89d3e602722b691df49498
juan-david-restrepo <davyz2006@gmail.com>
2025-02-13 17:38:29 -0500

    actulizacion

---
+++ clase-german-11feb/talleres/referencias/septimo.js
@@ -0,0 +1,16 @@
+
+// Uso de await con una Promesa
+// Crea una función asíncrona que utilice await para esperar una
+//  promesa que resuelve en 3 segundos y luego imprime "Proceso terminado".
+
+async function mensajeretrasado() {
+    setTimeout((resolve)=>{
+
+         await  
+    },3000)
+    
+}
+
+function name(params) {
+    
+}
