ea41017c7fe1ba6187efa1329613c0d1718bcb5c
juan-david-restrepo <davyz2006@gmail.com>
2025-03-05 17:40:41 -0500

    actuaizacion

---
+++ carlos/proyecto4/Biblioteca/Modelo/Entity/EntityLibros.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Entity
+{
+    public class EntityLibros
+    {
+        public int ID_libro { get; set; }
+        public string Titulo { get; set; }
+        public string contenido { get; set; }
+    }
+}
