bf36daf2480bb4d01fbae07cb3f51e1e1e69b90b
juan <alexzo8677@gmail.com>
2025-03-07 17:45:10 -0500

    actualizacion 7/03/2025

---
+++ proyecto4/ProyectoCapas/Modelo/ConexionMsql.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Modelo
+{
+    public class ConexionMsql
+    {
+        public MySqlConnection connection;
+        private string cadenaConexion;
+        
+        public ConexionMsql()
+        {
+            cadenaConexion = "Database=tiendadb;Datasoure=Localhost;User Id=root; Password=";
+            connection = new MySqlConnection(cadenaConexion);
+        }
+
+        public MySqlConnection GetConnection()
+        {
+
+            try
+            {
+                if (connection.State != System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return connection;
+            }
+        }
+}
